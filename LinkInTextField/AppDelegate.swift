7d93fc73ffdb44ad7c8702369c70c76c0c0ec60e
Mark Cornelisse <info@markcornelisse.nl>
2015-12-28 21:07:31 +0100

    Initial Commit

---
+++ LinkInTextField/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LinkInTextField
+//
+//  Created by Mark Cornelisse on 28/12/15.
+//  Copyright © 2015 Over de muur producties. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LinkInTextField/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LinkInTextField
+//
+//  Created by Mark Cornelisse on 28/12/15.
+//  Copyright © 2015 Over de muur producties. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
